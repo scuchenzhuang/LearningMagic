6a545c34ab132fd83968f059da05230ddb6ecda3
scuchenzhuang <1249879691@qq.com>
2022-11-29 15:54:59 +0800

    20221129

--- EightFactorDataVisulize/代码/daily_main.py
+++ EightFactorDataVisulize/代码/daily_main.py
@@ -20,9 +20,9 @@ from multiprocessing import Pool
 def main():
     '''此处修改今日日期，与之比较的上周数据'''
     '''今日'''
-    end_date = "20221118"
+    end_date = "20221124"
     '''与上周的哪一日进行比较'''
-    last_week = '20221111'
+    last_week = '20221117'
 
 
     path = os.getcwd()
